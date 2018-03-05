1569f6ef297c154533dbf6af826415bba6403ec8
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-05 11:52:16 -0600

    sqlite activity working, database created

---
+++ livecodes/customers.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports.customers = [
+    {
+        "id": 0,
+        "firstName": "Pittman",
+        "lastName": "Gibbs",
+        "address": "483 Marconi Place, Bentley, Virgin Islands, 2476"
+    },
+    {
+        "id": 1,
+        "firstName": "Martin",
+        "lastName": "Orr",
+        "address": "399 Rutherford Place, Bellamy, Florida, 816"
+    },
+    {
+        "id": 2,
+        "firstName": "Opal",
+        "lastName": "Wilkins",
+        "address": "171 Atlantic Avenue, Coldiron, Mississippi, 9283"
+    },
+    {
+        "id": 3,
+        "firstName": "Helene",
+        "lastName": "Briggs",
+        "address": "452 Ovington Avenue, Delshire, Montana, 5166"
+    },
+    {
+        "id": 4,
+        "firstName": "Lillian",
+        "lastName": "Peters",
+        "address": "890 Rutland Road, National, Utah, 3626"
+    },
+    {
+        "id": 5,
+        "firstName": "Jacquelyn",
+        "lastName": "Camacho",
+        "address": "944 Irving Place, Odessa, South Carolina, 4207"
+    }
+];
---
+++ livecodes/orders.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports.orders = [
+    {
+        "id": 0,
+        "customerId": 0,
+        "orderDate": "Thu Feb 11 2016 21:26:42 GMT-0600 (CST)",
+        "amount": 5.8709
+    },
+    {
+        "id": 1,
+        "customerId": 3,
+        "orderDate": "Mon Nov 19 1984 03:40:37 GMT-0600 (CST)",
+        "amount": 2.9602
+    },
+    {
+        "id": 2,
+        "customerId": 1,
+        "orderDate": "Fri Apr 09 1999 16:16:49 GMT-0500 (CDT)",
+        "amount": 4.9037
+    },
+    {
+        "id": 3,
+        "customerId": 2,
+        "orderDate": "Sun Jul 18 2004 16:28:30 GMT-0500 (CDT)",
+        "amount": 6.4002
+    },
+    {
+        "id": 4,
+        "customerId": 1,
+        "orderDate": "Fri Dec 31 1999 23:44:46 GMT-0600 (CST)",
+        "amount": 4.2654
+    },
+    {
+        "id": 5,
+        "customerId": 3,
+        "orderDate": "Sat Jul 09 1977 19:21:34 GMT-0500 (CDT)",
+        "amount": 8.607
+    }
+];
